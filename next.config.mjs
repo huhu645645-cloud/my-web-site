7a3157470077815463671fe57ae5102207b61122
huhu645645-cloud <huhu645645@gmail.com>
2026-02-04 10:16:19 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // 强制跳过检查，只保留这一项
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
